1840399db97e58191b85d3340c55842ed7584e80
victor.calisto <victor.calisto>
2024-10-13 16:22:45 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,34 @@ User.find_or_create_by!(email: 'chefe@examplo.com') do |user|
   user.password = '123456'
   user.password_confirmation = '123456'
 end
+
+##############################
+require 'faker'
+require 'i18n'
+
+def gerar_email(nome_completo)
+  nome_sem_acentos = I18n.transliterate(nome_completo.downcase)
+  mail = nome_sem_acentos.gsub(/\s+/, '.').gsub(/[^a-z.]/, '') + '@exemplo.com'
+  
+  mail
+end
+
+10.times do
+  nome_completo = Faker::Name.name
+
+  while nome_completo !~ /\A[a-zA-ZÀ-ÿ\s]+\z/
+    nome_completo = Faker::Name.name
+  end
+
+  email = gerar_email(nome_completo)
+
+  Contato.create!(
+    nome: nome_completo,
+    email: email,
+    telefone: "629#{rand(10000000..99999999)}",
+    whatsapp: "629#{rand(10000000..99999999)}", 
+    horario_preferencial: Time.zone.now.change(hour: rand(8..18), min: 0)
+  )
+end
+
+########################
